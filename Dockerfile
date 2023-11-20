087a5600c2b2d58a2dd2207f9b1e3dd9d78a322c
EDJINEDJA <edjinedjalaris@gmail.com>
2023-11-20 19:58:58 +0100

    update 201120231936

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.9
 ENV PROJECT_DIR /usr/local/bin/src/app
+RUN apt update -y && apt install awscli -y
 RUN pip install pipenv
 WORKDIR ${PROJECT_DIR}
 COPY . ${PROJECT_DIR}
--- src/mlProject/constants/__init__.py
+++ src/mlProject/constants/__init__.py
@@ -1,2 +1,5 @@
-"""_summary_
-"""
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
